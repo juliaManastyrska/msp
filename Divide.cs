87c6ec7f75ec38fe17804eabfc20f47ccee51680
amisfpm <tereshchenko.igor@gmail.com>
2016-09-06 21:23:52 +0300

    Update Divide.cs

--- Divide.cs
+++ Divide.cs
@@ -16,11 +16,11 @@
                 ;
             }
         }
-        public double variable_1 { get; set; }
-        public double variable_2 { get; set; }
+        public double variable_1 { get return 0; set; }
+        public double variable_2 { get return 0; set; }
         public int opp_id
         {
-            get { return0; }
+            get { return 0; }
             set { ; }
         }
 
